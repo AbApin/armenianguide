9d62be58271323643f6e174808921e14e27afc01
AbApin <apinyan2005@mail.ru>
2026-06-20 12:36:53 +0400

    aragacotn targmanutyun

--- assets/js/i18n.js
+++ assets/js/i18n.js
@@ -5,12 +5,23 @@
     return localStorage.getItem(LANG_KEY) || "hy";
   }
 
+  var i18nImages = {
+    marzer1: { hy: "./assets/img/marzer1.webp", en: "./assets/img/marzer1_en.webp", ru: "./assets/img/marzer1.webp" },
+    marzer2: { hy: "./assets/img/marzer2.webp", en: "./assets/img/marzer2_en.webp", ru: "./assets/img/marzer2.webp" }
+  };
+
   function applyTranslations(lang) {
     if (!window.translations || !translations[lang]) return;
     var t = translations[lang];
     document.querySelectorAll("[data-i18n]").forEach(function (el) {
       var key = el.getAttribute("data-i18n");
       if (t[key] !== undefined) el.textContent = t[key];
+    });
+    document.querySelectorAll("[data-i18n-img]").forEach(function (img) {
+      var key = img.getAttribute("data-i18n-img");
+      if (i18nImages[key] && i18nImages[key][lang]) {
+        img.src = i18nImages[key][lang];
+      }
     });
   }
 
